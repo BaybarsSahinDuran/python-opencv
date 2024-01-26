20aac842c16e28a2ed23e132bc6ab11166c8b82a
BaybarsSahinDuran <baybarssahin01@gmail.com>
2024-01-27 02:40:55 +0300

    first commit

---
+++ temel python/Python Koşul İfadeleri/if_else_yapisi.py
@@ -0,0 +1,12 @@
+username = 'baybarsduran'
+password = '1234'
+
+if (username == 'baybarsduran'): #Eğer 'if' sağlanırsa 'if' bloğuna girer
+
+    if (password == '12345'):
+        print('Hoş geldiniz')
+    else:
+        print('parola yanlis')
+        
+else: #'if' sağlanmaz ise kod 'else' bloğuna girer
+    print('username yanlis')
